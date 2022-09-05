7358eec17094bceb3d18c12935ba98d6d8b2a0cf
Jason Griffis <jgriffis1976@gmail.com>
2022-09-04 23:03:28 -0400

    Added Day-001 program, a simple band name generator.

---
+++ Day-001/Band_Name_Generator.py
@@ -0,0 +1,14 @@
+# Simple band name generator
+# Jason Griffis
+# 09/04/2022
+
+# Print a welcome message for the program
+print("Welcome to the Band Name Generator!")
+
+# Ask for input on a city and pet name to use for the band name
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What is your pets name?\n")
+
+# Combine the input variables to create the band name,
+# then print them out to the user
+print(f"You're band name is: {city_name} {pet_name}\n")
